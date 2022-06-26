bc81f832ada399864b5b9f25b5e25f1114832e6f
Rodrigo Sousa <58708951+rodrigosousadev@users.noreply.github.com>
2022-06-25 23:30:47 -0300

    Update q5proposto.py

--- 2avaliação/q5proposto.py
+++ 2avaliação/q5proposto.py
@@ -1,24 +1,20 @@
-from random import uniform
+#from random import uniform
 meses = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setenbro','outubro','novembro','dezembro']
 ano = []
-venda_mes = []
+v_mes = []
 total = 0
-#preenchimento da matriz
-for mes in range(12):
-    for semana in range(4):
-        venda_mes.append(uniform(0,1000))
-        #venda_mes.append(float(input(f'vendas da {semana+1}ª semana do mes de {meses[mes]}:')))
-    ano.append(venda_mes.copy())
-    total += sum(venda_mes)
-    venda_mes.clear()
-#apresentação dos dados infomados
-print(f'\n{"VENDAS NO ANO".center(67)}')
-print('=' * 68)
+for mes in range(0, 12):
+    for semana in range(0, 4):
+        #v_mes.append(uniform(0,1000))
+        v_mes.append(float(input(f'vendas da {semana+1}ª semana do mes de {meses[mes]}:')))
+    ano.append(v_mes.copy())
+    total += sum(v_mes)
+    v_mes.clear()
+print(f'\n{"VENDAS NO ANO".center(67)}','\n','=' * 68)
 print(f'{"Mês".center(10)} {"1ª semana".center(10)} {"2ª semana".center(10)} {"3ª semana".center(10)} {"4ª semana".center(10)} {"Total no mês".center(10)}')
 for mes in range(12):    
     print(f'{meses[mes].center(10)}', end ='|')
     for semana in range(4):
         print(f'{ano[mes][semana]:.2f}'.rjust(5).center(10), end ='|')
-    print(f'{sum(ano[mes]):.2f}'.rjust(5).center(12))
-print('=' * 68)    
-print(f'\nTotal anual: R${total:.2f}\n')
+    print(f'{sum(ano[mes]):.2f}'.rjust(5).center(12)) 
+print('=' * 68,f'\nTotal anual: R${total:.2f}\n')
